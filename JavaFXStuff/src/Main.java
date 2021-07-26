51ecc7988bfc0a899727a652a6b507a94698b42d
Benji-Young <benjaminty2003@gmail.com>
2021-07-26 12:04:49 +0100

    First JFrame

---
+++ JavaFXStuff/src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+public class Main extends JFrame{
+
+	public static void main(String[] args) {
+		
+		new Main();
+
+	}
+	
+	Main(){
+		setSize(600, 600);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+}
