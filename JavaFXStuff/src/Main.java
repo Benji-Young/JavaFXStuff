927e16882ed2a16edf2ce3fd82fc464b4f57d493
Benji-Young <benjaminty2003@gmail.com>
2021-07-29 10:40:13 +0100

    Added a blue JPanel

--- JavaFXStuff/src/Main.java
+++ JavaFXStuff/src/Main.java
@@ -1,6 +1,11 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.*;
 public class Main extends JFrame{
 
+	public Panel panel = new Panel();
+	
 	public static void main(String[] args) {
 		
 		new Main();
@@ -12,6 +17,7 @@ public class Main extends JFrame{
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		this.add(panel);
 	}
 
 }
---
+++ JavaFXStuff/src/Panel.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+import javax.swing.JPanel;
+public class Panel extends JPanel{
+
+	Panel(){
+		setBackground(Color.blue);
+	}
+	
+}
